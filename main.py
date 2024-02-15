2517e1d3a40f8125e92e24ddae265137ea11ccdd
Viktoria11512 <viktoriadavigora@knu.ua>
2024-02-15 23:49:05 +0200

    Додано функцію для перевірки на степінь п'ятірки і додано виклик функції в main.py

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ from utils import *
 
 n=int(input())
 print(n,factorial(n))
+
+number=int(input())
+print( number,is_power_of_5(number))
--- utils.py
+++ utils.py
@@ -3,3 +3,13 @@ def factorial(n):
         return 1
     else:
         return n * factorial(n-1)
+
+def is_power_of_5(number):
+    if number == 1:
+        return True
+    elif number < 1:
+        return False
+    else:
+        while number % 5 == 0:
+            number /= 5
+        return number == 1
