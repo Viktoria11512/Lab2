6d92ebe96497a9217904a9976eb63b85067e130e
Viktoria11512 <viktoriadavigora@knu.ua>
2024-02-16 00:01:16 +0200

    Додано функцію для обрахунку чи є число степенем двійки

--- main.py
+++ main.py
@@ -2,3 +2,12 @@ from utils import *
 
 n=int(input())
 print(n,factorial(n))
+<<<<<<< Updated upstream
+=======
+
+number=int(input())
+print( number,is_power_of_5(number))
+
+num=int(input())
+print( num,is_power_of_two(num))
+>>>>>>> Stashed changes
